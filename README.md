956d8403a2b79980c3e887d6dadd5f0f4491e57f
Henri Bouvier <henri@omsignal.com>
2014-09-20 17:10:33 -0400

    added support for sets

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # REST API, Key Value Pair Store, backed by MongoDB for Tomahawk
+
+## To use this plugin
+
+    npm install -g tomahawk-routes-kv-store
+    npm install -g tomahawk-plugin-mongo
+
+Then create a configuration file in your home directory:
+
+    ~/.tomahawk/config.json
+    {
+        "plugins" : {
+            "store" : {
+                "context"        : "/store/api/v1",
+                "interval"       : 1000,
+                "implementation" : "tomahawk-plugin-mongo",
+                "url"            : "mongodb://localhost:27017/tomahawk"
+            },
+            "store-route" : {
+                "implementation" : "tomahawk-routes-kv-store"
+            }
+        }
+    }
--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,44 @@ var store = require('../lib/store.js')();
 
 store.connect('mongodb://localhost/test');
 
-store.set('name', 'Bob Smith', function (err, result) {
-   store.get('name', function (err, document) {
-      console.log('document:', document);
-      store.close();
-   });
+
+function testKV(next) {
+	store.set([{key:'name', value:'Bob Smith'}], function (err, value) {
+		console.log('set name "Bob Smith": err:', err, ', value:', value);
+		store.get('name', function (err, value) {
+			console.log('get name: err:', err, ', value:', value);
+		    store.del('name', function (err, value) {
+				console.log('del name: err:', err, ', value:', value);
+		    	next();
+		    });
+	   });
+	});
+}
+
+function testSET(next) {
+	store.sadd([{key:'MySet', value:'Bob'}], function (err, value) {
+		console.log('sadd MySet Bob: err:', err, ', value:', value);
+		store.sget('MySet', undefined, function (err, values) {
+			console.log('sget MySet: err:', err, ', values:', values);
+			store.sget('MySet', 'Boby', function (err, value) {
+				console.log('sget Boby: err:', err, ', value:', value);
+				store.sget('MySet', 'Bob', function (err, value) {
+					console.log('sget Bob: err:', err, ', value:', value);
+					store.sdel('MySet', 'Bob', function (err, value) {
+						console.log('sdel Bob: err:', err, ', value:', value);
+						store.sget('MySet', 'Bob', function (err, value) {
+							console.log('sget Bob: err:', err, ', value:', value);
+							next();
+						});
+					});
+				});
+			});
+		});
+	});
+}
+
+testKV(function () {
+	testSET(function () {
+		store.close();
+	});
 });
